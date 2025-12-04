3eb7fc7f1df54776e35f3dd33e3f9a80d6f84053
Glenn Griffin <Glenn Griffin ggriffiniii@gmail.com>
2025-12-04 09:34:58 -0800

    day 13 part 2

--- src/day13.rs
+++ src/day13.rs
@@ -33,3 +33,63 @@ fn solve_d13_p1(input: &str) -> usize {
         );
     min_wait_time.bus_id * min_wait_time.wait_time
 }
+
+// Compute the modular inverse t such that a*t ≡ 1 mod n
+// https://en.wikipedia.org/wiki/Extended_Euclidean_algorithm#Modular_integers
+fn inverse(a: isize, n: isize) -> isize {
+    let mut t = 0;
+    let mut r = n;
+    let mut newt = 1;
+    let mut newr = a;
+
+    while newr != 0 {
+        let q = r / newr;
+        let prev_t = t;
+        t = newt;
+        newt = prev_t - q * newt;
+        let prev_r = r;
+        r = newr;
+        newr = prev_r - q * newr;
+    }
+
+    if r > 1 {
+        panic!("{a} is not invertible");
+    }
+    if t < 0 {
+        t += n;
+    }
+    t
+}
+
+// All the bus numbers are prime numbers. Use the chinese remainder theorem:
+// https://www.youtube.com/watch?v=MdePzlQtnCc
+#[aoc(day13, part2)]
+fn solve_d13_p2(input: &str) -> usize {
+    let (_, bus_ids) = input.split_once('\n').unwrap();
+    let mut ri = Vec::new();
+    let mut mods: Vec<usize> = Vec::new();
+    let mut unique_mod = 0;
+    for (time_after, bus_id) in bus_ids.split(',').enumerate() {
+        if bus_id == "x" {
+            continue;
+        }
+        let bus_id: usize = bus_id.parse().unwrap();
+        mods.push(bus_id);
+        ri.push((bus_id - time_after % bus_id) % bus_id);
+    }
+
+    let unique_mod: usize = mods.iter().product();
+    let mi: Vec<_> = mods.iter().map(|m| unique_mod / m).collect();
+    let xi: Vec<_> = mods
+        .iter()
+        .zip(mi.iter())
+        .map(|(&m, &mi): (&usize, &usize)| inverse(mi as isize, m as isize) as usize)
+        .collect();
+
+    ri.iter()
+        .copied()
+        .zip(mi.iter().copied().zip(xi.iter().copied()))
+        .map(|(ri, (mi, xi))| ri * mi * xi)
+        .sum::<usize>()
+        % unique_mod
+}
